6b46aa2fcdf14c37788aea26cf5f012f69e404ce
denis-larin-22 <denislarin2017@gmail.com>
2023-03-22 21:16:23 +0200

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import ReduxCounter from './components/ReduxCounter.jsx';
+import { Routes, Route, NavLink } from "react-router-dom";
+import Main from "./pages/Main";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import NotFound from './pages/NotFound';
 
  
 function App() {
   return (
     <div className="App">
       <ReduxCounter />
+
+      <h6>#######################</h6>
+
+      <NavLink to='/'>main </NavLink>
+      <NavLink to='/Contact'>contact </NavLink>
+      <NavLink to='/About'>about </NavLink>
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/About" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
     </div>
   );
 }
